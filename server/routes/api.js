07b361002aaea82325fdbdf6b214c49e19a98871
Max Levtov <maxlevtov@gmail.com>
2017-03-27 00:18:36 +0300

    prod

---
+++ server/routes/api.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+
+/* GET api listing. */
+router.get('/', (req, res) => {
+  
+  res.sendFile(path.join(__dirname, '../data.json'));
+  
+});
+
+module.exports = router;
