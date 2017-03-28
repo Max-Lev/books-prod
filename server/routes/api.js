b23e1534335c80eae34b18ed66d9674edbd29799
Max Levtov <maxlevtov@gmail.com>
2017-03-28 21:14:47 +0300

    name fix

--- server/routes/api.js
+++ server/routes/api.js
@@ -4,9 +4,7 @@ const path = require('path');
 
 /* GET api listing. */
 router.get('/', (req, res) => {
-  
   res.sendFile(path.join(__dirname, '../data.json'));
-  
 });
 
 module.exports = router;
